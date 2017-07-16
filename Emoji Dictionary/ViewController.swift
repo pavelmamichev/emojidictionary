d164f515f8f18d20940d353e42dd8a4c44fa9500
Pavel Mamichev <pavel.mamichev@gmail.com>
2017-07-16 01:38:46 +0100

    moveSegue

--- Emoji Dictionary/ViewController.swift
+++ Emoji Dictionary/ViewController.swift
@@ -34,6 +34,10 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         return cell
         
     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "moveSegue", sender: "🐣")
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
